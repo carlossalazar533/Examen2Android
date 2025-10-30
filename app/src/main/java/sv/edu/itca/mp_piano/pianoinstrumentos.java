7e2736a940ed7daf167e661c00aaef49fafa0cf7
carlossalazar533 <blaky503salazar@gmail.com>
2025-10-29 19:54:07 -0600

    Primer commit del proyecto

--- app/src/main/java/sv/edu/itca/mp_piano/pianoinstrumentos.java
+++ app/src/main/java/sv/edu/itca/mp_piano/pianoinstrumentos.java
@@ -10,44 +10,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class pianoinstrumentos extends AppCompatActivity implements View.OnClickListener{
+public class pianoinstrumentos extends AppCompatActivity implements View.OnClickListener {
+
     private SoundPool soundPoolI;
-
-    private int soundGuitar;
-    private int soundDrum;
-    private int soundFlauta;
-    private int soundViolin;
-    private int soundArmonica;
-    private int soundTrompeta;
-    private int soundHarp;
-
-
-
+    private int soundGuitar, soundDrum, soundFlauta, soundViolin, soundArmonica, soundTrompeta, soundHarp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pianoinstrumentos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        inciarSonidos();
-        iniciarBonotnes();
+        iniciarSonidos();
+        iniciarBotones();
     }
 
-    private void iniciarBonotnes()
-    {
+    private void iniciarBotones() {
         findViewById(R.id.btnGuitar).setOnClickListener(this);
         findViewById(R.id.btntambor).setOnClickListener(this);
         findViewById(R.id.btntompeta).setOnClickListener(this);
@@ -55,11 +35,9 @@ public class pianoinstrumentos extends AppCompatActivity implements View.OnClick
         findViewById(R.id.btnviolin).setOnClickListener(this);
         findViewById(R.id.btnArmonica).setOnClickListener(this);
         findViewById(R.id.btnharp).setOnClickListener(this);
-
     }
 
-    private void inciarSonidos()
-    {
+    private void iniciarSonidos() {
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
@@ -70,117 +48,55 @@ public class pianoinstrumentos extends AppCompatActivity implements View.OnClick
                 .setAudioAttributes(audioAttributes)
                 .build();
 
-        // Cargar los archivos de sonido desde la carpeta raw
-        soundGuitar = soundPoolI.load(this, R.raw.guitar, 1);
-        soundDrum = soundPoolI.load(this, R.raw.drum, 1);
-        soundArmonica = soundPoolI.load(this, R.raw.armonica, 1);
-        soundTrompeta = soundPoolI.load(this, R.raw.trompeta, 1);
-        soundHarp = soundPoolI.load(this, R.raw.harp, 1);
-        soundViolin = soundPoolI.load(this, R.raw.violin, 1);
-        soundFlauta = soundPoolI.load(this, R.raw.flute, 1);
+        // Cargar sonidos
+        soundGuitar = safeLoad(R.raw.guitar);
+        soundDrum = safeLoad(R.raw.drum);
+        soundArmonica = safeLoad(R.raw.armonica);
+        soundTrompeta = safeLoad(R.raw.trompeta);
+        soundHarp = safeLoad(R.raw.harp);
+        soundViolin = safeLoad(R.raw.violin);
+        soundFlauta = safeLoad(R.raw.flute);
+    }
+
+    private int safeLoad(int resId) {
+        try {
+            return soundPoolI.load(this, resId, 1);
+        } catch (Exception e) {
+            return 0;
+        }
     }
 
     @Override
-    public void onClick(View view)
-    {
+    public void onClick(View view) {
         int id = view.getId();
-        if (id == R.id.btnGuitar) {
-            tocarNota("Guitarra", soundGuitar);
-        } else if (id == R.id.btntambor) {
-            tocarNota("Tambor", soundDrum);
-        } else if (id == R.id.btntompeta) {
-            tocarNota("Trompeta", soundTrompeta);
-        } else if (id == R.id.btnviolin) {
-            tocarNota("Violin", soundViolin);
-        } else if (id == R.id.btnflauta) {
-            tocarNota("Flauta", soundFlauta);
-        } else if (id == R.id.btnArmonica) {
-            tocarNota("Armonica", soundArmonica);
-        } else if (id == R.id.btnharp) {
-            tocarNota("Arpa", soundHarp);
-        }
+        if (id == R.id.btnGuitar) tocarNota("Guitarra", soundGuitar);
+        else if (id == R.id.btntambor) tocarNota("Tambor", soundDrum);
+        else if (id == R.id.btntompeta) tocarNota("Trompeta", soundTrompeta);
+        else if (id == R.id.btnviolin) tocarNota("Violin", soundViolin);
+        else if (id == R.id.btnflauta) tocarNota("Flauta", soundFlauta);
+        else if (id == R.id.btnArmonica) tocarNota("Armonica", soundArmonica);
+        else if (id == R.id.btnharp) tocarNota("Arpa", soundHarp);
 
-        aplicarEfectoVisual((Button) view);
-
-
+        if (view instanceof Button) aplicarEfectoVisual((Button) view);
     }
 
-    private void tocarNota(String nombreNota, int soundId) {
-        // Reproducir sonido si está cargado
+    private void tocarNota(String nombre, int soundId) {
         if (soundId != 0) {
-            soundPoolI.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+            soundPoolI.play(soundId, 1f, 1f, 1, 0, 1f);
         } else {
-            // Mientras no tengas los sonidos, muestra un toast
-            Toast.makeText(this, "♪ " + nombreNota + " ♪", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "♪ " + nombre + " ♪", Toast.LENGTH_SHORT).show();
         }
     }
+
     private void aplicarEfectoVisual(Button boton) {
-        // Efecto de presión
         boton.animate()
                 .scaleX(0.95f)
                 .scaleY(0.95f)
                 .setDuration(100)
-                .withEndAction(() -> {
-                    boton.animate()
-                            .scaleX(1.0f)
-                            .scaleY(1.0f)
-                            .setDuration(100)
-                            .start();
-                })
+                .withEndAction(() -> boton.animate().scaleX(1f).scaleY(1f).setDuration(100).start())
                 .start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return true;
-
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-
-        if (itemId == R.id.piano) {
-
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-
-
-        } else if (itemId == R.id.intrumentos) {
-            mostrarMensaje("Modo Instrumentos activado");
-            return true;
-        } else if (itemId == R.id.animales) {
-
-            Intent intent = new Intent(this, pianoselva.class);
-            startActivity(intent);
-            finish();
-            return true;
-        }
-        else if (itemId == R.id.grupo)
-        {
-            Intent intent = new Intent(this,Participantes.class);
-            startActivity(intent);
-            finish();
-            return true;
-
-        }
-
-        else if (itemId == R.id.salir)
-        {
-            finishAffinity();
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void mostrarMensaje(String mensaje) {
-        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
-    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -188,5 +104,34 @@ public class pianoinstrumentos extends AppCompatActivity implements View.OnClick
             soundPoolI.release();
             soundPoolI = null;
         }
+    }
+
+    // -------------------- MENU --------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.piano) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else if (id == R.id.intrumentos) {
+            Toast.makeText(this, "Modo Instrumentos activado", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.animales) {
+            startActivity(new Intent(this, pianoselva.class));
+            finish();
+        } else if (id == R.id.grupo) {
+            startActivity(new Intent(this, Participantes.class));
+            finish();
+        } else if (id == R.id.salir) {
+            finishAffinity();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/sv/edu/itca/mp_piano/pianoselva.java
+++ app/src/main/java/sv/edu/itca/mp_piano/pianoselva.java
@@ -10,104 +10,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class pianoselva extends AppCompatActivity implements View.OnClickListener {
-    private SoundPool soundPoolA;
 
-    private int soundCow;
-    private int soundDog;
-    private int soundCat;
-    private int soundPig;
-    private int soundDuck;
-    private int soundHen;
-    private int soundHorse;
+    private SoundPool soundPoolA;
+    private int soundCow, soundDog, soundCat, soundHorse, soundDuck, soundHen, soundPig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pianoselva);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
 
-
-
-        });
         iniciarSonidos();
-        configurarBotones();
+        iniciarBotones();
     }
 
-    private void configurarBotones()
-    {
+    private void iniciarBotones() {
+        findViewById(R.id.btncow).setOnClickListener(this);
         findViewById(R.id.btndog).setOnClickListener(this);
+        findViewById(R.id.btncat).setOnClickListener(this);
+        findViewById(R.id.btnhorse).setOnClickListener(this);
         findViewById(R.id.btnduck).setOnClickListener(this);
         findViewById(R.id.btnhen).setOnClickListener(this);
-        findViewById(R.id.btnhorse).setOnClickListener(this);
-        findViewById(R.id.btncat).setOnClickListener(this);
-        findViewById(R.id.btncow).setOnClickListener(this);
         findViewById(R.id.btnpig).setOnClickListener(this);
-
-
-
-    }
-    @Override
-    public void onClick(View v)
-    {
-        int id = v.getId();
-
-        if (id == R.id.btndog) {
-            tocarNota("Perro", soundDog);
-        } else if (id == R.id.btnduck) {
-            tocarNota("Pato", soundDuck);
-        } else if (id == R.id.btnhen) {
-            tocarNota("Gallina", soundHen);
-        } else if (id == R.id.btnhorse) {
-            tocarNota("Caballo", soundHorse);
-        } else if (id == R.id.btncat) {
-            tocarNota("Gato", soundCat);
-        } else if (id == R.id.btncow) {
-            tocarNota("Vaca", soundCow);
-        } else if (id == R.id.btnpig) {
-            tocarNota("Cerdo", soundPig);
-        }
-        aplicarEfectoVisual((Button) v);
-
-    }
-    private void tocarNota(String nombreNota, int soundId) {
-        // Reproducir sonido si está cargado
-        if (soundId != 0) {
-            soundPoolA.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
-        } else {
-            // Mientras no tengas los sonidos, muestra un toast
-            Toast.makeText(this, "♪ " + nombreNota + " ♪", Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void aplicarEfectoVisual(Button boton) {
-        // Efecto de presión
-        boton.animate()
-                .scaleX(0.95f)
-                .scaleY(0.95f)
-                .setDuration(100)
-                .withEndAction(() -> {
-                    boton.animate()
-                            .scaleX(1.0f)
-                            .scaleY(1.0f)
-                            .setDuration(100)
-                            .start();
-                })
-                .start();
     }
 
-    private void iniciarSonidos()
-    {
+    private void iniciarSonidos() {
         AudioAttributes audioAttributes = new AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
@@ -118,18 +48,57 @@ public class pianoselva extends AppCompatActivity implements View.OnClickListene
                 .setAudioAttributes(audioAttributes)
                 .build();
 
-        // Cargar los archivos de sonido desde la carpeta raw
-        soundDog = soundPoolA.load(this, R.raw.dog, 1);
-        soundDuck = soundPoolA.load(this, R.raw.duck, 1);
         soundCow = soundPoolA.load(this, R.raw.cow, 1);
+        soundDog = soundPoolA.load(this, R.raw.dog, 1);
         soundCat = soundPoolA.load(this, R.raw.cat, 1);
-        soundPig = soundPoolA.load(this, R.raw.pig, 1);
-        soundHen = soundPoolA.load(this, R.raw.hen, 1);
         soundHorse = soundPoolA.load(this, R.raw.horse, 1);
-
-
+        soundDuck = soundPoolA.load(this, R.raw.duck, 1);
+        soundHen = soundPoolA.load(this, R.raw.hen, 1);
+        soundPig = soundPoolA.load(this, R.raw.pig, 1);
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btncow) tocarNota("Vaca", soundCow);
+        else if (id == R.id.btndog) tocarNota("Perro", soundDog);
+        else if (id == R.id.btncat) tocarNota("Gato", soundCat);
+        else if (id == R.id.btnhorse) tocarNota("Caballo", soundHorse);
+        else if (id == R.id.btnduck) tocarNota("Pato", soundDuck);
+        else if (id == R.id.btnhen) tocarNota("Gallina", soundHen);
+        else if (id == R.id.btnpig) tocarNota("Cerdo", soundPig);
+
+        if (view instanceof Button) aplicarEfectoVisual((Button) view);
+    }
+
+    private void tocarNota(String nombre, int soundId) {
+        if (soundId != 0) {
+            soundPoolA.play(soundId, 1f, 1f, 1, 0, 1f);
+        } else {
+            Toast.makeText(this, "♪ " + nombre + " ♪", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void aplicarEfectoVisual(Button boton) {
+        boton.animate()
+                .scaleX(0.95f)
+                .scaleY(0.95f)
+                .setDuration(100)
+                .withEndAction(() -> boton.animate().scaleX(1f).scaleY(1f).setDuration(100).start())
+                .start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (soundPoolA != null) {
+            soundPoolA.release();
+            soundPoolA = null;
+        }
+    }
+
+    // -------------------- MENU --------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
@@ -138,46 +107,23 @@ public class pianoselva extends AppCompatActivity implements View.OnClickListene
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
+        int id = item.getItemId();
 
-        if (itemId == R.id.piano) {
-
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+        if (id == R.id.piano) {
+            startActivity(new Intent(this, MainActivity.class));
             finish();
-
-        } else if (itemId == R.id.intrumentos) {
-            Intent intent = new Intent(this, pianoinstrumentos.class);
-            startActivity(intent);
+        } else if (id == R.id.intrumentos) {
+            startActivity(new Intent(this, pianoinstrumentos.class));
             finish();
-        } else if (itemId == R.id.animales) {
-            mostrarMensaje("Modo Animales activado");
-            return true;
-
-        } else if (itemId == R.id.grupo) {
-            Intent intent = new Intent(this,Participantes.class);
-            startActivity(intent);
+        } else if (id == R.id.animales) {
+            Toast.makeText(this, "Modo Animales activado", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.grupo) {
+            startActivity(new Intent(this, Participantes.class));
             finish();
-            return true;
-
-        }
-        else if (itemId == R.id.salir)
-        {
+        } else if (id == R.id.salir) {
             finishAffinity();
-            return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    private void mostrarMensaje(String mensaje) {
-        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (soundPoolA != null) {
-            soundPoolA.release();
-            soundPoolA = null;
-        }
     }
 }
